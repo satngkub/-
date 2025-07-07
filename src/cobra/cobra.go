625c5a0cc59ecc93c19a2562cb1d9772c368c076
SATNG4404 <satngxgg@gmail.com>
2025-07-07 08:24:50 +0700

    Update cobra.go

--- src/cobra/cobra.go
+++ src/cobra/cobra.go
@@ -1,78 +1,86 @@
-package ccgo
+// main.go
+package main
 
 import (
-	"cc-go-attack/src"
-	"errors"
 	"fmt"
-	"github.com/fatih/color"
-	"github.com/spf13/cobra"
-	"os"
-	"strconv"
+	"log"
+	"os" // import package os เพื่อเข้าถึง Command Line Arguments
+	"strconv" // import package strconv สำหรับแปลง string เป็น int
+	"cc-go-attack/src" // เปลี่ยนตาม Path ของ package src ของคุณ
 )
 
-var (
-	workers  	string
-	times    	string
-	ccGoCmd = &cobra.Command{
-		Use:          "cc-go",
-		Short:        "cc-go for simple cc attack",
-		Example:      "cc-go -w 100 -c 20 http://www.******.com/cc?id=1",
-		SilenceUsage: true,
-		Args: func(cmd *cobra.Command, args []string) error {
-			if len(args) < 1 {
-				return errors.New("requires target url arg (http://xxxxx.com)")
-			}
-			return nil
-		},
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return do(args)
-		},
+func main() {
+	// ตรวจสอบจำนวน arguments ที่ส่งมา
+	// os.Args[0] คือชื่อโปรแกรม (main.go)
+	// os.Args[1] คือ URL
+	// os.Args[2] คือ Worker (ถ้ามี)
+	// os.Args[3] คือ Time (ถ้ามี)
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <URL> [workers] [time_in_seconds]")
+		fmt.Println("Example: go run main.go https://www.example.com 50000 60")
+		os.Exit(1) // ออกจากโปรแกรมพร้อมรหัสข้อผิดพลาด
 	}
-)
 
-func init() {
-	ccGoCmd.PersistentFlags().StringVarP(&workers, "worker", "w", "100", "The number of worker threads executing concurrently")
-	ccGoCmd.PersistentFlags().StringVarP(&times, "time", "t", "10", "How long(s)")
-}
+	url := os.Args[1] // ดึง URL จาก Argument แรก
 
-func do(args []string) error {
-	cc := new(src.CC)
+	// กำหนดค่าเริ่มต้นสำหรับ Worker และ Time
+	// คุณสามารถปรับค่าเริ่มต้นเหล่านี้ได้ตามความเหมาะสม
+	defaultWorkers := 50000
+	defaultTime := 60
 
-	cc.Url = args[0]
-	num, _ := strconv.Atoi(workers)
-	cc.Worker = num
+	// ตรวจสอบและดึงค่า Worker จาก Argument ที่สอง (ถ้ามี)
+	workers := defaultWorkers
+	if len(os.Args) > 2 {
+		parsedWorkers, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			log.Printf("Warning: Invalid workers value '%s'. Using default %d workers.\n", os.Args[2], defaultWorkers)
+		} else {
+			workers = parsedWorkers
+		}
+	}
 
-	t, _ := strconv.Atoi(times)
-	cc.Time = t
+	// ตรวจสอบและดึงค่า Time จาก Argument ที่สาม (ถ้ามี)
+	timeInSeconds := defaultTime
+	if len(os.Args) > 3 {
+		parsedTime, err := strconv.Atoi(os.Args[3])
+		if err != nil {
+			log.Printf("Warning: Invalid time value '%s'. Using default %d seconds.\n", os.Args[3], defaultTime)
+		} else {
+			timeInSeconds = parsedTime
+		}
+	}
 
-	err := cc.New()
+
+	loadTester := &src.CC{
+		Url:    url, // ใช้ URL ที่ดึงมาจาก Command Line Argument
+		Worker: workers,
+		Time:   timeInSeconds,
+	}
+
+	err := loadTester.New()
 	if err != nil {
-		return err
+		log.Fatalf("Failed to initialize load tester: %v", err)
 	}
 
-	cc.Start()
-	//cc.Scheduler.Done()
+	fmt.Println("Starting load test...")
+	fmt.Printf("Target URL: %s\n", loadTester.Url)
+	fmt.Printf("Workers: %d\n", loadTester.Worker)
+	fmt.Printf("Duration: %d seconds\n", loadTester.Time)
+	fmt.Println("--------------------------------------------------")
 
-	printReport(cc)
-	return nil
-}
+	loadTester.Start()
 
-func Exec() {
-	if err := ccGoCmd.Execute(); err != nil {
-		os.Exit(0)
+	loadTester.Done() // เรียก ShutDown เมื่อโปรแกรมรันครบเวลา
+	fmt.Println("\nLoad test finished.")
+	fmt.Printf("--------------------------------------------------\n")
+	fmt.Printf("Summary:\n")
+	fmt.Printf("  Total Requests Sent: %d\n", loadTester.Report.TotalRequests)
+	fmt.Printf("  Successful Requests: %d\n", loadTester.Report.SuccessCount)
+	fmt.Printf("  Failed Requests:     %d\n", loadTester.Report.ErrorCount)
+
+	if loadTester.Report.TotalRequests > 0 {
+		fmt.Printf("  Overall RPS (Average): %.2f req/s\n", float64(loadTester.Report.TotalRequests)/float64(loadTester.Time))
 	}
 }
 
-func printReport(cc *src.CC)  {
-	fmt.Println()
-	fmt.Println()
-	color.Cyan("|------------------------------------------------------------------|")
-	color.Cyan("| attack url: "+ cc.Url)
-	color.Cyan("|------------------------------------------------------------------|")
-	color.Cyan("| total times: "+ times)
-	color.Cyan("|------------------------------------------------------------------|")
-	color.Cyan("| concurrent workers: "+ workers)
-	color.Cyan("|------------------------------------------------------------------|")
-	color.Cyan("| total requests: "+strconv.FormatInt(int64(cc.Report.Request), 10))
-	color.Cyan("|------------------------------------------------------------------|")
-}
